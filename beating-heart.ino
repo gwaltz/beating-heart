f18c5f44315fd489441ec73870da9feb874978d5
George Waltz <78068559+gwaltz@users.noreply.github.com>
2023-08-30 08:43:18 -0700

    Add files via upload

---
+++ beating-heart.ino
@@ -0,0 +1,25 @@
+#define potpin A0
+#define ledpin 3
+#define test 4
+
+unsigned long testmillis, currentmillis;
+unsigned long h = 1000;
+int brightness = 0, faderateon, faderateoff;
+
+unsigned long prevmillis, interval;
+
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(ledpin, OUTPUT);
+  pinMode(test, OUTPUT);
+  millis();
+}
+
+void loop() {
+  // Read the value from the potentiometer
+  currentmillis = millis();
+  heartbeat();
+  // Map the potentiometer value to LED brightness (0-255)
+}
+
